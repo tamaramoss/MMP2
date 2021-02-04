bc61c84e02dcc03753ac92ef3061367a222f6b0d
SP00KBOOK\c007b <fhs44416@fh-salzburg.ac.at>
2021-02-04 21:21:00 +0100

    add menu screens and button assets

---
+++ assets/btnPlay.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="btnPlay" tilewidth="369" tileheight="176" tilecount="2" columns="1">
+ <image source="btnPlay.png" width="369" height="352"/>
+</tileset>
---
+++ assets/mainMenu.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="mainMenu" tilewidth="64" tileheight="65" tilecount="435" columns="29">
+ <image source="mainMenu.png" width="1857" height="982"/>
+</tileset>
